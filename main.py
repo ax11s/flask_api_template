ba306fd85d60a921fd1cdcded05f0fec8145ed0a
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-08 21:09:43 +0100

    update the example

--- main.py
+++ main.py
@@ -33,5 +33,34 @@ def random_string():
     return random_line
 
 
+# Define route and API function
+@app.route('/neverhaveiever')
+def neverhaveiever():
+    # Api key aunthentiffication
+    reckey = request.headers.get('key')
+    
+    if reckey != apikey:
+        return jsonify({'error': 'Invalid API key'}), 401
+    
+
+    #getting the string
+    with open("neverhaveiever.txt", "r") as file:
+        lines = file.readlines()
+
+    index = random.randint(0, len(lines))
+    print(index)
+    index = index - 1
+    random_line = lines[index]
+
+    print(random_line)
+
+    file.close()
+    return random_line
+
+
+
+
 if __name__ == '__main__':
     app.run()
+
+
