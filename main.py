6f368cea0fee0cd064b551e73e18badb54224925
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-08 09:51:39 +0100

    Added a file randomizer
    Not working at the moment

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Define your API key
+
+# Define your API key
+apikey = "testkey"
+
+# Define your route and API function
+
+# Define your route and API function
+@app.route('/boredom')
+def random_string():
+    # Api key aunthentiffication
+    reckey = request.headers.get('key')
+    
+    if reckey != apikey:
+        return jsonify({'error': 'Invalid API key'}), 401
+    
+
+    #getting the string
+    with open("boredom.txt", "r") as file:
+        lines = file.readlines()
+
+    index = random.randint(0, len(lines))
+    index = index - 1
+    random_line = lines[index]
+
+    print(random_line)
+
+    file.close()
+
+
+if __name__ == '__main__':
+    app.run()
